6d3a62bff89359db624bd92b34ef6d507b4ab7e3
Ghada Bennasr <gh.bennasr@gmail.com>
2024-11-27 23:51:51 +0100

    debug

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,14 +6,12 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full px-6 py-8">
       <div className="flex items-center justify-between">
-        {/* Logo on the left */}
         <div className="logo-container">
           <Link to="/">
             <img src={assets.hand_logo} alt="My Logo" className="h-9 w-auto" />
           </Link>
         </div>
 
-        {/* Centered link */}
         <div className="absolute left-1/2 transform -translate-x-1/2">
           <Link
             to="/"
@@ -23,7 +21,6 @@ const Navbar = () => {
           </Link>
         </div>
 
-        {/* Right-aligned links */}
         <div className="flex space-x-8">
           <Link
             to="/about"
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-family-SohneBreit font-semibold">
-        Hello, Tailwind CSS!
-      </h1>
-      <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-        Click Me
-      </button>
-    </div>
+    <div className="h-screen flex flex-col items-center justify-center"></div>
   );
 };
 
