22056453ecfacc916b58604107f5be9f85f574f2
Ghada Bennasr <gh.bennasr@gmail.com>
2024-11-27 23:45:20 +0100

    navbar layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="fixed top-0 left-0 w-full px-6 py-8">
+      <div className="flex items-center justify-between">
+        {/* Logo on the left */}
+        <div className="logo-container">
+          <Link to="/">
+            <img src={assets.hand_logo} alt="My Logo" className="h-9 w-auto" />
+          </Link>
+        </div>
 
-export default Navbar
+        {/* Centered link */}
+        <div className="absolute left-1/2 transform -translate-x-1/2">
+          <Link
+            to="/"
+            className="nav-link uppercase font-sohne text-black font-semibold text-sm"
+          >
+            selected works
+          </Link>
+        </div>
+
+        {/* Right-aligned links */}
+        <div className="flex space-x-8">
+          <Link
+            to="/about"
+            className="nav-link uppercase font-sohne text-black font-semibold text-sm"
+          >
+            About
+          </Link>
+          <Link
+            to="/contact"
+            className="nav-link uppercase font-sohne text-black font-semibold text-sm"
+          >
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sohne: ["SohneBreit", "sans-serif"],
+      },
+      fontWeight: {
+        light: 200,
+        semibold: 500,
+        book: 600,
+        bold: 700,
+      },
+    },
   },
   plugins: [],
 };
