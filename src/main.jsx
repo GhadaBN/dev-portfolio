b2d16ca0d33b514a56b969c20124d93c85daf8dc
Ghada Bennasr <gh.bennasr@gmail.com>
2024-11-27 18:11:14 +0100

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import NotFound from "./pages/NotFound/NotFound.jsx";
+import Home from "./pages/Home/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects/:projectId" element={<Project />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
