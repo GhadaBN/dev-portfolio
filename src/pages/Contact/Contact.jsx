eaf2543eca8299d574d5e71411bc03422ae86f0b
Ghada Bennasr <gh.bennasr@gmail.com>
2024-12-01 07:10:57 +0100

    contact-about

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,53 +4,16 @@ import { FiArrowUpRight } from "react-icons/fi";
 
 const Contact = () => {
   return (
-    <div className="relative h-[calc(90vh-9vh)] overflow-hidden">
+    <div className="relative h-[calc(90vh-50vh)] overflow-hidden">
       <div className="absolute left-4 sm:left-0 md:bottom-8 md:left-4 lg:left-6 w-[90vw]">
         <div className="relative text-4xl md:text-customXl font-sohne font-book leading-tight-0.9 uppercase whitespace-nowrap flex items-center space-x-2">
-          <span>GHADA</span>
-          <span
-            className="w-60 bg-customBeige inline-block align-middle"
-            style={{ height: "10vh" }}
-          ></span>
-          <span>BENNASR</span>
-          <Link
-            to="/about"
-            className="flex items-center space-x-1 group relative -top-5"
-          >
-            <span className="h-4 w-4 rounded-full border border-black bg-customBeige group-hover:bg-customRed"></span>
-            <span className="font-sohneBreit font-semibold text-customSm uppercase">
-              About
-            </span>
-          </Link>
+          <span>Have a project in mind?</span>
         </div>
-
         <div className="relative text-4xl md:text-customXl font-sohne font-book leading-tight-0.9 uppercase whitespace-nowrap flex items-center space-x-2">
-          <a
-            href={"https://github.com/GhadaBN"}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center space-x-1 group ml-2 relative -top-5"
-          >
-            <span className="font-sohneBreit font-semibold text-customSm uppercase">
-              GITHUB
-            </span>
-            <span className="relative h-4 w-4 rounded-full border border-black bg-customBeige group-hover:bg-customRed flex items-center justify-center">
-              {" "}
-              <FiArrowUpRight className="text-black group-hover:text-white h-3 w-3" />
-            </span>
-          </a>
-
-          <span>CREATIVE DEVELOPER</span>
+          <span>Or want to say hello,</span>
         </div>
-
-        <div className="text-4xl md:text-customXl font-sohne leading-tight-0.9 font-book uppercase">
-          <div className="flex items-center">
-            <span className="relative text-customXl -top-3">+</span>
-            <span className="ml-2">VISUAL DESIGNER</span>
-          </div>
-        </div>
-        <div className="flex items-center text-4xl md:text-customXl leading-tight-0.9 font-sohne font-book uppercase whitespace-nowrap">
-          <span>BASED IN BERLIN</span>
+        <div className="relative text-4xl md:text-customXl font-sohne font-book leading-tight-0.9 uppercase whitespace-nowrap flex items-center space-x-2">
+          <span>drop me a line.</span>
         </div>
       </div>
     </div>
