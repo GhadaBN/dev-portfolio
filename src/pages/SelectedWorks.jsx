ceab4d2eb77ce30da2dcdf14ad6a4e3e1f203ee2
Ghada Bennasr <gh.bennasr@gmail.com>
2024-12-03 03:44:33 +0100

    projects

--- src/pages/SelectedWorks.jsx
+++ src/pages/SelectedWorks.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiArrowUpRight } from "react-icons/fi";
+import { FiArrowRight } from "react-icons/fi";
 import { assets } from "../assets/assets";
 import projects from "../assets/projectsData.json";
 
 const SelectedWorks = () => {
   return (
     <div className="selected-works-page-wrapper relative h-[calc(90vh-9vh)] overflow-hidden">
-      <div className="absolute bottom-8 left-8 flex flex-col items-start text-left w-[90vw]">
+      <div className="absolute bottom-12 left-8 flex flex-col items-start text-left w-[90vw]">
         {projects.map((project, index) => (
           <div
             key={project.id}
@@ -17,6 +17,9 @@ const SelectedWorks = () => {
               to={`/projects/${project.id}`}
               className="flex items-center space-x-1 group relative"
             >
+              <span className="relative  flex items-center justify-center">
+                <FiArrowRight className="text-black group-hover:text-white h-20 w-20" />
+              </span>
               <div className="overflow-hidden">
                 <span
                   className="inline-block transform translate-y-full animate-maskReveal text-customLg font-sohne font-book leading-x-tight uppercase whitespace-nowrap"
@@ -25,9 +28,6 @@ const SelectedWorks = () => {
                   {project.title}
                 </span>
               </div>
-              <span className="relative h-4 w-4 rounded-full border border-black bg-customBeige group-hover:bg-customRed flex items-center justify-center">
-                <FiArrowUpRight className="text-black group-hover:text-white h-3 w-3" />
-              </span>
             </Link>
           </div>
         ))}
