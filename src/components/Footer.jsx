05e8d8ab50a051f1d0c975aca5a2650ddf5f8e8c
Ghada Bennasr <gh.bennasr@gmail.com>
2024-12-05 15:31:31 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer className="fixed bottom-0 left-0 w-full h-14 px-8 py-8 mb-2 z-20">
+      <div className="flex items-center justify-end space-x-1">
+        <span className="h-5 w-5 rounded-full border border-black bg-customPink"></span>
+        <span className="h-5 w-5 rounded-full border border-black  bg-customBeige"></span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
